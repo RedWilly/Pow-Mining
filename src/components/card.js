1ffe74547267d5ccd7d2b460754e3beefba69bae
RedWilly <thompsonau023@gmail.com>
2022-10-21 10:40:53 -0400

    Colorcode

--- src/components/card.js
+++ src/components/card.js
@@ -31,7 +31,7 @@ export const Card = (props) => {
         height="280"
       ></img>
       <div className="mt-4 flex-col justify-center items-center">
-        <p className="bold ml-[-100px]">Mutants #{props.tokenId}</p>
+        <p className="bold ml-[-100px]">POW Pirates #{props.tokenId}</p>
         <button className="stake-button bg-black w-[100%] rounded-3xl px-24 mt-3 mb-1 py-2" onClick={() => { stake(props.tokenId) }}>
           Stake
         </button>
