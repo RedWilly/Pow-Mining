a8ab5d09905e8338f4bd77b7354b427019a1a33f
RedWilly <thompsonau023@gmail.com>
2022-11-10 05:43:05 -0400

    Update Header.js

--- src/utils/Header.js
+++ src/utils/Header.js
@@ -1,14 +1,18 @@
 export const MenuItemList = [
   {
-    label: "Home",
+    label: "HOME",
     link: "https://powpirates.com/",
   },
   {
-    label: "Litepaper",
+    label: "LITEPAPER",
     link: "https://powpirates.com/WhitePaper",
   },
   {
-    label: "Marketplace",
+    label: "MARKETPLACE",
     link: "https://nuwton.io/asset/EthereumPow/0x9fed227e39460614b41206cdb1cb37363393f60f/",
+  },
+  {
+    label: "FARM",
+    link: "https://powpirates.com/Farms",
   },
 ];
